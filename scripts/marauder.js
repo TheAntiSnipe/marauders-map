7474200f6208f67560c85d6f090b719783d264b1
Mihir Kulkarni <38178591+TheAntiSnipe@users.noreply.github.com>
2021-01-29 11:46:57 +0530

    Added all normal teleport points so that they load when the webpage loads

--- scripts/marauder.js
+++ scripts/marauder.js
@@ -1,10 +1,22 @@
 let stringOfCoordinates = '';
+import { teleporterLocations } from './coordinates.js'
 
-const addMarker = (x,y) => {
-    const coordinates = `[${x},${y}],`
-    stringOfCoordinates+=coordinates;
-    console.log(stringOfCoordinates);
-    L.marker([x,y]).addTo(map);
+const displaySprite = (iconData,coordinateList) => {
+  for (let i of coordinateList) {
+    addMarker(iconData,...i);
+  }
+} 
+
+const addMarker = (iconData = '',x,y) => {
+    if(!iconData) {
+      const coordinates = `[${x},${y}],`
+      stringOfCoordinates+=coordinates;
+      console.log(stringOfCoordinates);
+      L.marker([x,y]).addTo(map);
+    }
+    else {
+      L.marker([x,y],{icon:iconData}).addTo(map);
+    }
 }
 
 const positionHandler = (MouseEvent) => {
@@ -29,7 +41,7 @@ var map = L.map('mapid', {
 // dimensions of the image
 var w = 5120,
   h = 5120,
-  url = './python-scripts/final-merged-map.jpg';
+  url = '../python-scripts/final-merged-map.jpg';
 
 // calculate the edges of the image, in coordinate space
 var southWest = map.unproject([0, h], map.getMaxZoom() - 1);
@@ -43,5 +55,14 @@ L.imageOverlay(url, bounds).addTo(map);
 
 // tell leaflet that the map is exactly as big as the image
 map.setMaxBounds(bounds);
+const iconData = L.icon(
+  {
+    iconUrl: '../marker-asset-files/marker-teleport.png',
+    iconSize:[30,30],
+    iconAnchor:[0,0],
+    popupAnchor:[0,0]
+  }
+);
+displaySprite(iconData,teleporterLocations);
 
 map.on('click',positionHandler)
