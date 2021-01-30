0334f0f3b8f50036006ba03f0cdd258fa81347e4
Mihir Kulkarni <38178591+TheAntiSnipe@users.noreply.github.com>
2021-01-30 09:13:51 +0530

    Added teleporter locations, and created a custom class so that future assets will be easier to load in.

--- scripts/marauder.js
+++ scripts/marauder.js
@@ -1,5 +1,7 @@
 let stringOfCoordinates = '';
-import { teleporterLocations } from './coordinates.js'
+// Has a string formatted in a way that you can copy-paste it directly from console to coordinates.js
+// so that new variables can be added flexibly. Dev feature, pretty useless to end user though.
+import { teleporterLocations,statueOfSevenLocations } from './coordinates.js'
 
 const displaySprite = (iconData,coordinateList) => {
   for (let i of coordinateList) {
@@ -7,8 +9,8 @@ const displaySprite = (iconData,coordinateList) => {
   }
 } 
 
-const addMarker = (iconData = '',x,y) => {
-    if(!iconData) {
+const addMarker = (iconData,x,y) => {
+    if(iconData=='') {
       const coordinates = `[${x},${y}],`
       stringOfCoordinates+=coordinates;
       console.log(stringOfCoordinates);
@@ -24,13 +26,13 @@ const positionHandler = (MouseEvent) => {
   const y = MouseEvent.latlng.lng;
   const markerSet = confirm("Add this marker?");
   if(markerSet) {
-    addMarker(x,y);
+    addMarker('',x,y);
   } else {
     return;
   }
 }
 
-var map = L.map('mapid', {
+const map = L.map('mapid', {
   minZoom: 1,
   maxZoom: 4,
   center: [0, 0],
@@ -39,15 +41,15 @@ var map = L.map('mapid', {
 });
 
 // dimensions of the image
-var w = 5120,
-  h = 5120,
-  url = '../python-scripts/final-merged-map.jpg';
+const width = 5120;
+const height = 5120;
+const url = '../python-scripts/final-merged-map.jpg';
 
 // calculate the edges of the image, in coordinate space
-var southWest = map.unproject([0, h], map.getMaxZoom() - 1);
-var northEast = map.unproject([w, 0], map.getMaxZoom() - 1);
+const southWest = map.unproject([0, height], map.getMaxZoom() - 1);
+const northEast = map.unproject([width, 0], map.getMaxZoom() - 1);
 // console.log(southWest,northEast)
-var bounds = new L.LatLngBounds(southWest, northEast);
+const bounds = new L.LatLngBounds(southWest, northEast);
 
 // add the image overlay, 
 // so that it covers the entire map
@@ -55,14 +57,17 @@ L.imageOverlay(url, bounds).addTo(map);
 
 // tell leaflet that the map is exactly as big as the image
 map.setMaxBounds(bounds);
-const iconData = L.icon(
-  {
-    iconUrl: '../marker-asset-files/marker-teleport.png',
+const customIcon = L.Icon.extend({
+  options: {
     iconSize:[30,30],
     iconAnchor:[0,0],
     popupAnchor:[0,0]
   }
-);
-displaySprite(iconData,teleporterLocations);
+});
+
+const teleporterIcon = new customIcon({iconUrl: '../marker-asset-files/marker-teleport.png'})
+const SoSIcon = new customIcon({iconUrl: '../marker-asset-files/marker-SoS.png'})
+displaySprite(teleporterIcon,teleporterLocations);
+displaySprite(SoSIcon,statueOfSevenLocations);
 
 map.on('click',positionHandler)
