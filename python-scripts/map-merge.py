f23202386b6dc04c142fd6382654492148b69c2f
Mihir Kulkarni <38178591+TheAntiSnipe@users.noreply.github.com>
2021-03-13 09:07:31 +0530

    Added readability fixes and removed redundant imports

--- python-scripts/map-merge.py
+++ python-scripts/map-merge.py
@@ -1,63 +1,67 @@
-import numpy as np
 from PIL import Image
 
 
-def merge_images_horizontally(imgs, coordinate):
+def mergeImagesHorizontally(images, coordinate):
     # create two lists - one for heights and one for widths
-    widths, heights = zip(*(i.size for i in imgs))
-    width_of_new_image = sum(widths)
-    height_of_new_image = min(heights)  # take minimum height
+    widths, heights = zip(*(i.size for i in images))
+    resultWidth = sum(widths)
+    resultheight = min(heights)  # take minimum height
     # create new image
-    new_im = Image.new('RGB', (width_of_new_image, height_of_new_image))
-    new_pos = 0
-    for im in imgs:
-        new_im.paste(im, (new_pos, 0))
-        new_pos += im.size[0]  # position for the next image
-    new_im.save('final'+str(coordinate)+'.jpg')
+    resultImage = Image.new('RGB', (resultWidth, resultheight))
+    nextImagePosition = 0
+    for im in images:
+        resultImage.paste(im, (nextImagePosition, 0))
+        nextImagePosition += im.size[0]  # position for the next image
+    resultImage.save('final'+str(coordinate)+'.jpg')
 
 
-def merge_images_vertically(imgs):
+def mergeImagesVertically(images):
     # create two lists - one for heights and one for widths
-    widths, heights = zip(*(i.size for i in imgs))
-    width_of_new_image = min(widths)  # take minimum width
-    height_of_new_image = sum(heights)
+    widths, heights = zip(*(i.size for i in images))
+    resultWidth = min(widths)  # take minimum width
+    resultheight = sum(heights)
     # create new image
-    new_im = Image.new('RGB', (width_of_new_image, height_of_new_image))
-    new_pos = 0
-    for im in imgs:
-        new_im.paste(im, (0, new_pos))
-        new_pos += im.size[1]  # position for the next image
-    new_im.save('final-merged-map.jpg')
+    resultImage = Image.new('RGB', (resultWidth, resultheight))
+    nextImagePosition = 0
+    for im in images:
+        resultImage.paste(im, (0, nextImagePosition))
+        nextImagePosition += im.size[1]  # position for the next image
+    resultImage.save('final-merged-map.jpg')
 
 
 def main():
-    '''This code works in two parts. The first part generates horizontal slices
-    while the second part merges all the horizontal slices into the complete map.'''
+    """This code works in two parts.
+
+    The first part generates horizontal slices while the second part
+    merges all the horizontal slices into the complete map.
+    """
     listOfRows = []
     for x in range(-10, 10):
-        '''Here, we generate a list of lists, with each list element containing
-        one row of filepaths of the 20x20 grid, making for a total of 20 rows.'''
-        image_row = []
+        """Here, we generate a list of lists, with each list element containing
+        one row of filepaths of the 20x20 grid, making for a total of 20
+        rows."""
+        imageRow = []
         for y in range(-10, 10):
             filepath = 'tile-'+str(y)+'_'+str(x)+'.jpg'
-            image_row.append(filepath)
-        listOfRows.append(image_row)
+            imageRow.append(filepath)
+        listOfRows.append(imageRow)
 
     listOfMergedRows = []
 
     for x in range(20):
-        '''Here, we loop through the list of rows and horizontally merge each row,
-        and generate a list of filepaths of horizontal strips for the next step.'''
+        '''Here, we loop through the list of rows and horizontally
+        merge each row, and generate a list of filepaths of
+        horizontal strips for the next step.'''
         imgs = [Image.open(im) for im in listOfRows[x]]
-        merge_images_horizontally(imgs, x)
+        mergeImagesHorizontally(imgs, x)
 
-        '''Since we traverse from -10 to 10, we reverse the filepath order 
+        '''Since we traverse from -10 to 10, we reverse the filepath order
         for the final merge so as not to end up with a reversed merge'''
         listOfMergedRows.append('final'+str(19-x)+'.jpg')
 
-    # Finally, we merge the horizontal strip into one complete image
-    imgs = [Image.open(im) for im in listOfMergedRows]
-    merge_images_vertically(imgs)
+        # Finally, we merge the horizontal strip into one complete image
+        images = [Image.open(im) for im in listOfMergedRows]
+        mergeImagesVertically(images)
 
 
 if __name__ == '__main__':
