fbcaaed9e77c87af362a6f3317fdc69e743055b7
TheAntiSnipe <38178591+TheAntiSnipe@users.noreply.github.com>
2023-02-15 12:20:45 -0500

    Linted comments.

--- scripts/marauder.js
+++ scripts/marauder.js
@@ -1,6 +1,13 @@
 import { teleporterLocations, statueOfSevenLocations } from "./coordinates.js";
-//This imports all the coordinate data from the specifically made file coordinates.js. Since there's a lot of coordinates, I decided to use this approach to make the code look cleaner.
+//This imports all the coordinate data from the specifically made file coordinates.js. 
+//Since there's a lot of coordinates, I decided to use this approach to make the code look cleaner.
 
+
+// Renders a marker with a certain image icon to the image.
+// If that payload is empty (dev functionality), it simply
+// logs the position of the click using console.log.
+// This is used when we are looking to add coordinates to
+// coordinates.js.
 const addMarker = (x, y, iconData = "") => {
   if (iconData == "") {
     console.log(x, y);
@@ -12,8 +19,8 @@ const addMarker = (x, y, iconData = "") => {
     L.marker([x, y], { icon: iconData }).addTo(map);
   }
 };
-//Used in two cases. Once for dev reasons (logs coordinates where clicks happen) through positionHandler, once from displaySprite where it gets a custom icon object. Thus, I've used a default parameter.
 
+//Gets a mouse event and runs addMarker with default parameters over it.
 const positionHandler = (MouseEvent) => {
   window.stringOfCoordinates = "";
   const x = MouseEvent.latlng.lat;
@@ -25,8 +32,8 @@ const positionHandler = (MouseEvent) => {
     return;
   }
 };
-//Gets a mouse event and runs addMarker with default parameters over it
 
+//Creates a custom icon object using the iconUrl and sends it to addMarker
 const renderCustomIcon = (iconUrl, locationData) => {
   const customIcon = L.Icon.extend({
     options: {
@@ -38,11 +45,12 @@ const renderCustomIcon = (iconUrl, locationData) => {
   const iconToBeRendered = new customIcon({ iconUrl });
   locationData.forEach((element) => addMarker(...element, iconToBeRendered));
 };
-//Creates a custom icon object using the iconUrl and sends it to displaySprite
 
+// Generates a map object, assigns it to window so that it's globally accessible, 
+// then defines all of its properties. Called by buildEnvironment.
 const initializeMap = () => {
   window.map = L.map("mapid", {
-    minZoom: 1,
+    minZoom: 2,
     maxZoom: 4,
     center: [0, 0],
     zoom: 1,
@@ -62,14 +70,16 @@ const initializeMap = () => {
   // tell leaflet that the map is exactly as big as the image
   map.setMaxBounds(bounds);
 };
-//Generates a map object, assigns it to window so that it's globally accessible, then defines all of its properties. Called by buildEnvironment
 
+//First calls initializeMap, then runs the renderCustomIcon function 
+// for each icon to be rendered. Also adds a click listener with 
+// positionHandler as a callback.
 const buildEnvironment = () => {
   initializeMap();
   renderCustomIcon("../marker-asset-files/teleport.png", teleporterLocations);
   renderCustomIcon("../marker-asset-files/sos.png", statueOfSevenLocations);
   map.on("click", positionHandler);
 };
-//First calls initializeMap, then runs the renderCustomIcon function for each icon to be rendered. Also adds a click listener with positionHandler as a callback
+
 
 buildEnvironment();
